7f3cf96aa48dff7a8abc7fb937ee23ee1f746c6b
Efim Trofimov <53835219+Gre7@users.noreply.github.com>
2022-10-18 18:33:20 +0300

    build/added react-types

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./components/App/App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
     <App />
